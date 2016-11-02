6653102a3601aa3ac3de35c3ed727572e1c9caf9
azhenplus <15163804576@163.com>
2016-11-02 09:09:15 +0800

    充值记录的退出按钮变长

--- Manchong/gulpfile.js
+++ Manchong/gulpfile.js
@@ -7,13 +7,13 @@ gulp.task('lessIn',function(){
     gulp.src('less/less.less')
         .pipe(less())
         .pipe(gulp.dest('css'))
-})
+});
 
 gulp.task('img',function(){
     gulp.src('./img/*')
         .pipe(imageisux('./image',true))
-})
+});
 
 gulp.task('watchLess',function(){
     gulp.watch('less/*.less',['lessIn'])
-})
+});
